13750294e7c379a5b72852ae15493713bc79a1c4
unknown <himanshuj053.com>
2025-11-24 18:54:13 +0530

    Added

---
+++ Q31.java
@@ -0,0 +1,52 @@
+/*
+                                     50. Pow(x, n)
+Medium
+Topics
+premium lock icon
+Companies
+Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+
+ 
+
+Example 1:
+
+Input: x = 2.00000, n = 10
+Output: 1024.00000
+Example 2:
+
+Input: x = 2.10000, n = 3
+Output: 9.26100
+Example 3:
+
+Input: x = 2.00000, n = -2
+Output: 0.25000
+Explanation: 2-2 = 1/22 = 1/4 = 0.25
+  
+*/
+
+
+public class Q31 {
+    public static void main(String[] args) {
+        double x = 2.00000;
+        int n = 10;
+        System.out.println(myPow(x, n));
+    }
+
+    public static double myPow(double x, int n) {
+        if (n == 0) return 1;
+
+        // Handle negative powers
+        if (n < 0) {
+            x = 1 / x;
+            n = -n;
+        }
+
+        // Recursion with divide and conquer (Fast Power)
+        double half = myPow(x, n / 2);
+
+        if (n % 2 == 0)
+            return half * half;
+        else
+            return half * half * x;
+    }
+}
